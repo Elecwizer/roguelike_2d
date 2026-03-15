b0d034f9a92d6eef0e494d87e5cbcc6bb58d6e2e
Elecwizer <elecwizer@gmail.com>
2026-03-15 23:51:10 +0300

    added player character and player movement

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -8,17 +8,40 @@ public class BoardManager : MonoBehaviour
         public bool Passable;
     }
 
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        if (cellIndex.x < 0 || cellIndex.x >= _width
+            || cellIndex.y < 0 || cellIndex.y >= _height)
+        {
+            return null;
+        }
+
+        return m_BoardData[cellIndex.x, cellIndex.y];
+    }
+
+
+    public PlayerController Player;
+
     CellData[,] m_BoardData;
     Tilemap m_Tilemap;
+    Grid m_Grid;
 
     [SerializeField] int _width;
     [SerializeField] int _height;
     [SerializeField] Tile[] _groundTiles;
     [SerializeField] Tile[] _wallTiles;
+
+    public Vector3 CellToWorld(Vector2Int cellIndex)
+    {
+        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
+    }
+
     void Start()
     {
         m_BoardData = new CellData[_width, _height];
+
         m_Tilemap = GetComponentInChildren<Tilemap>();
+        m_Grid = GetComponentInChildren<Grid>();
 
         for (int y = 0; y < _height; ++y)
         {
@@ -41,6 +64,8 @@ public class BoardManager : MonoBehaviour
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
+
+        Player.Spawn(this, new Vector2Int(1,1));
     }
 
     void Update()
